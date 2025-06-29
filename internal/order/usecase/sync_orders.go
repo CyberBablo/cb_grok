3b92d5fe7081254130a535fbe0d70f14ae5469d9
RASIM <gadjiew.rasim@gmail.com>
2025-06-29 17:30:06 +0300

    extra log in sync orders

--- internal/order/usecase/sync_orders.go
+++ internal/order/usecase/sync_orders.go
@@ -3,6 +3,7 @@ package usecase
 import (
 	order_model "cb_grok/internal/order/model"
 	"context"
+	"fmt"
 	"go.uber.org/zap"
 	"time"
 )
@@ -34,12 +35,14 @@ func (u *usecase) SyncOrders(ctx context.Context) {
 					continue
 				}
 				if int(exchangeStatus) != order.StatusID {
+					u.log.Info(fmt.Sprintf("STARTED ORDER SYNCING %s", order.ExtID))
 					err := u.repo.UpdateOrderStatus(order.ID, int(exchangeStatus))
 					if err != nil {
 						u.log.Error("failed to update order status", zap.String("order_id", order.ExtID), zap.Error(err))
 						continue
 					}
 					if order.StatusID == int(order_model.OrderStatusFilled) {
+						u.log.Info(fmt.Sprintf("ORDER FILLED %s", order.ExtID))
 						quoteQty, err := u.ex.GetOrderQuoteQty(order.ExtID)
 						if err != nil {
 							u.log.Error("failed to update order quoteQty", zap.String("order_id", order.ExtID), zap.Error(err))
